e0f167721b4e8a6164d409263a027aa8c7b0fc50
Kurt Russelle Marmol <jkurtrussellemarmol@gmail.com>
2022-09-24 18:35:30 +0800

    Covid19 PH API

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const PORT = process.env.PORT || 3000;
+
+// Router
+const covidRouter = require('./covid19');
+
+// Middleware
+app.use(morgan('dev'))
+app.use('/', covidRouter)
+app.use((req, res, next) => {
+  const error = new Error('endpoint not found');
+  error.status = 404;
+  next(error)
+})
+
+// Error Handling
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  res.status(status).send({ message: err.message });
+});
+
+// Server Listening
+app.listen(PORT, () => {
+  console.log(`Server is now lisrening on port ${PORT}`)
+});
---
+++ covid19.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+const covidRouter = express.Router();
+
+const body = {
+  operationName: null,
+  variables: {},
+  query: "{\n  covidCaseStatus: covid_case_status {\n    active\n    all\n    asymptomatic\n    critical\n    died\n    mild\n    repatriate\n    recovered\n    severe\n    __typename\n  }\n  covidCaseLastUpdated: covid_case_last_updated {\n    updatedAt\n    __typename\n  }\n}\n"
+}
+
+covidRouter.get('/', async (req, res, next) => {
+  try {
+    const response = await fetch('https://dbph-gov-gql-bl4faseyeq-de.a.run.app/', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(body)
+    });
+    if (response.ok) {
+      const json = await response.json();
+      res.json(json.data)
+    }
+  } catch (err) {
+      console.log(err)
+    }
+})
+
+module.exports = covidRouter;
